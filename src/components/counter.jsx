760d34dd3d19303c7dc43e9854150ee491835f1a
Raymond Akalonu <raymondosy1@gmail.com>
2019-07-02 23:58:13 +0100

    Fix bug on deleting a counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,6 +17,7 @@ class Counter extends Component {
             <span className={classes}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm" onClick={this.handleIncrement}>Increment</button>
             <button className="btn btn-danger btn-sm m-2" onClick={() => this.props.onDelete(this.props.id)}>Delete</button>
+            {console.log(this.props)}
         </div>
     );
   }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -12,14 +12,20 @@ class Counters extends Component {
           { id: 4, value: 0 }
       ]
   };
-  handleDelete = () => {
+  handleDelete = (counterId) => {
     const counters = this.state.counters.filter(c => c.id !== counterId);
     this.setState({ counters })
 }
   render() {
     return (
       <div>
-          {this.state.counters.map(counter => <Counter key={counter.id} value={counter.value} onDelete={this.handleDelete}/>)}
+          {this.state.counters.map(counter => <Counter
+            key={counter.id}
+            value={counter.value} 
+            onDelete={this.handleDelete}
+            id={counter.id}
+          />
+          )}
       </div>
     );
   }
