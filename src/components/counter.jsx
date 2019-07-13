58541e76ce29b8d62dbb5000d1179b54b28915fa
Raymond Akalonu <raymondosy1@gmail.com>
2019-07-13 21:05:36 +0100

    update components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,36 +1,32 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-  };
-
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.props.counter.value === 0) ? "warning" : "primary";
+    return classes;
   }
-
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? 'Zero' : value;
+  }
   render() {
-      let classes = this.getBadgeClasses();
-
+    console.log('Counter - Rendered');
     return (
         <div>
-            <span className={classes}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm" onClick={this.handleIncrement}>Increment</button>
-            <button className="btn btn-danger btn-sm m-2" onClick={() => this.props.onDelete(this.props.id)}>Delete</button>
-            {console.log(this.props)}
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button
+              className="btn btn-secondary btn-sm"
+              onClick={this.props.onIncrement(this.props.counter)}>
+              Increment
+            </button>
+            <button
+              className="btn btn-danger btn-sm m-2"
+              onClick={() => this.props.onDelete(this.props.counter.id)}>
+              Delete
+            </button>
         </div>
     );
-  }
-
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.value === 0) ? "warning" : "primary";
-        return classes;
-    }
-
-  formatCount() {
-    const { value } = this.state;
-    return value === 0 ? 'Zero' : this.state.value;
   }
 }
 
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,31 +4,34 @@ import Counter from "./counter";
 
 // I have to fix this bug. I want to dynamically delete each element in the list using the delete button. Visit mosh 50:00
 class Counters extends Component {
-  state = {
-      counters: [
-          { id: 1, value: 4 },
-          { id: 2, value: 0 },
-          { id: 3, value: 0 },
-          { id: 4, value: 0 }
-      ]
-  };
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters })
-}
   render() {
+    const {counters, onReset, onDelete, onIncrement} = this.props;
+    console.log('Counters - Rendered');
     return (
       <div>
-          {this.state.counters.map(counter => <Counter
-            key={counter.id}
-            value={counter.value} 
-            onDelete={this.handleDelete}
-            id={counter.id}
-          />
-          )}
+        <button
+          onClick={onReset}
+          className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
+        {counters.map(counter => (
+        <Counter
+          key={counter.id}
+          onDelete={onDelete}
+          onIncrement={onIncrement}
+          counter={counter}
+        />
+        ))}
       </div>
     );
   }
 }
 
+
+/**
+ * The bug am having now is that, once the page rerenders and the state changes
+ * the increament button is called accross all buttons
+ * Then when i try to set the state of the new counter after it has been incremented,
+ * it throws an error concerning the use of states
+*/
 export default Counters;
